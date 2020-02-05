4b7e554b185a524ad62bffc2e5f0187f533b584b
ssaraff98 <ssaraff@purdue.edu>
2020-02-05 14:34:06 -0500

    Commented C.RELU

--- EAST_Detector/Architecture.py
+++ EAST_Detector/Architecture.py
@@ -7,22 +7,26 @@ class PVAnet(nn.Module):
         super(PVAnet, self).__init__()
 
     def CRelu(self, x, c):
+        # Convolution 1 - 7x7 kernel, 16 output channels, 1/2 * input size
         convolution1 = nn.Conv2d(in_channels=c, out_channels=16, kernel_size=7, stride=2, padding=3, bias=False)
         nn.init.xavier_uniform_(convolution1.weight)
+
         normalization = nn.BatchNorm2d(num_features=16)
         input = normalization(convolution1(x))
-        negation = torch.mul(input=input, other=-1)
-        concatenation = torch.cat((input, negation), 1)
+
+        # Concatenation of positive and negative features
+        concatenation = torch.cat((input, -input), 1)
+
+        # Convolution 2 - 1x1 kernel, 16 output channels, 1 * input size
         convolution2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=1, bias=True)
         input = convolution2(input)
+
         activation = nn.ReLU()
         return activation(input)
 
     def forward(self, x):
-        print(x.size())
         c = x.size()[1]
         x = self.CRelu(x, c)
-        print(x.size())
         pool1 = nn.MaxPool2d(kernel_size=3, stride=2)
         layer1 = pool1(x)
         return x
