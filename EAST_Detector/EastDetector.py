885e5fb20de11f57ca96abfa9251f883ce7be891
ssaraff98 <ssaraff@purdue.edu>
2020-02-12 00:31:18 -0500

    Implemented all the CRelu layers for PVAnet with logs of tensor sizes at each operation

--- EAST_Detector/EastDetector.py
+++ EAST_Detector/EastDetector.py
@@ -6,10 +6,13 @@ import torch.utils.data as data
 from torchvision import transforms
 import numpy as np
 from PIL import Image
-from Dataset import *
-from Architecture import PVAnet
+from Architecture import *
+
+import logging
 
 def main():
+    logging.basicConfig(filename="CreluTest.log", level=logging.INFO)
+    logging.info("Started")
     batch_size = 2
     training_path = "./Samples/"
     transform = transforms.Compose([
@@ -20,11 +23,16 @@ def main():
     training_data = torchvision.datasets.ImageFolder(root=training_path, transform=transform)
     training_loader = data.DataLoader(training_data, batch_size=batch_size, shuffle=True,  num_workers=4)
 
-    net = PVAnet()
+    net = CRelu()
 
     for i, train in enumerate(training_data, 0):
+        logging.info("Image #{}".format(i))
         inputs, labels = train
         outputs = net.forward(inputs.unsqueeze(0))
+        #logging.info("Output of CRelu:\n{}".format(outputs))
+        logging.info("\n")
+
+    logging.info("Finished")
 
 if __name__ == '__main__':
     main()
