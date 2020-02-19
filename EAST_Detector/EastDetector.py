3e5c740ee182eed646a8ef1837a498f80ad4dda2
ssaraff98 <ssaraff@purdue.edu>
2020-02-18 23:55:04 -0500

    Implemented feature merging branch of EAST

--- EAST_Detector/EastDetector.py
+++ EAST_Detector/EastDetector.py
@@ -6,33 +6,32 @@ import torch.utils.data as data
 from torchvision import transforms
 import numpy as np
 from PIL import Image
-from Architecture import *
+from PVAnet import *
+from Unet import *
 
 import logging
 
 def main():
-    logging.basicConfig(filename="CreluTest.log", level=logging.INFO)
-    logging.info("Started")
+    logging.basicConfig(filename="UnetTest.log", level=logging.INFO)
+    logging.info("\nStarted")
     batch_size = 2
     training_path = "./Samples/"
     transform = transforms.Compose([
-                    transforms.Resize(192),
+                    transforms.Resize((192, 192)),
                     transforms.ToTensor(),
                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
                 ])
     training_data = torchvision.datasets.ImageFolder(root=training_path, transform=transform)
     training_loader = data.DataLoader(training_data, batch_size=batch_size, shuffle=True,  num_workers=4)
 
-    net = CRelu()
-
     for i, train in enumerate(training_data, 0):
-        logging.info("Image #{}".format(i))
+        logging.info("Image #{}\n".format(i))
         inputs, labels = train
-        outputs = net.forward(inputs.unsqueeze(0))
-        #logging.info("Output of CRelu:\n{}".format(outputs))
-        logging.info("\n")
+        features = PVAnet(inputs)
+        output = Unet(features)
+        logging.info("-------------------------------------------------")
 
-    logging.info("Finished")
+    logging.info("Finished\n")
 
 if __name__ == '__main__':
     main()
